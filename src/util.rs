56ba0cfc28ed1fcd19b07e1e6ab4655ef7a5c2c2
Sebastian Köln <sebk@rynx.org>
2018-03-04 03:28:15 +0100

    .

--- src/util.rs
+++ src/util.rs
@@ -7,9 +7,9 @@ pub fn load<T>(path: &str) -> T
     where T: for <'de> Deserialize<'de> + Default + Sized
 {
     let mut buf = BufReader::new(File::open(path).expect("could not open file"));
-    deserialize_from(&mut buf, Infinite).unwrap_or_else(|_| T::default())
+    deserialize_from(&mut buf).unwrap_or_else(|_| T::default())
 }
 pub fn save<T: Serialize>(path: &str, data: &T) {
     let mut buf = BufWriter::new(File::create(path).expect("could not create file"));
-    serialize_into(&mut buf, data, Infinite).expect("Invalid data");
+    serialize_into(&mut buf, data).expect("Invalid data");
 }
